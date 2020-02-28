26403d821840eca1cb796fe75b934dce7c3932b8
root <root@bastion.8029.example.opentlc.com>
2020-02-28 06:31:30 +0000

    SITE-ENV-CREDENTIALS.SH - SCRIPT FOR CONFIG TOWER INCLUDING ENVIRONMENT VARIABLES

---
+++ site-env-credentials.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#export OSP_GUID="f4c8"
+#export TOWER_GUID="8029"
+#export OPENTLC_LOGIN="miguel.rubioroman-atos.net"
+#export OPENTLC_PASSWORD="Op3n7LC.71"
+#export OPENTLC_KEY_PATH="/root/.ssh/${TOWER_GUID}key.pem"
+#export GITHUB_REPO="https://github.com/gitjmrubio/ansible_advance_homework"
+#export JQ_REPO_BASE="http://www.opentlc.com/download/ansible_bootcamp"
+#export REGION="us-east-1"
+#export RH_MAIL_ID="miguel.rubioroman"
+OSP_GUID="f4c8"
+TOWER_GUID="8029"
+OPENTLC_LOGIN="miguel.rubioroman-atos.net"
+OPENTLC_PASSWORD="Op3n7LC.71"
+OPENTLC_KEY_PATH="/root/.ssh/${TOWER_GUID}key.pem"
+GITHUB_REPO="https://github.com/gitjmrubio/ansible_advance_homework"
+JQ_REPO_BASE="http://www.opentlc.com/download/ansible_bootcamp"
+REGION="us-east-1"
+RH_MAIL_ID="miguel.rubioroman"
+#ANSIBLE_ADVANCE_GUID=""
+
+# EXECUTE SITE-CONFIG-TOWER PLAYBOOK
+ansible-playbook site-config-tower.yml -e tower_GUID=${TOWER_GUID} -e osp_GUID=${OSP_GUID} -e opentlc_login=${OPENTLC_LOGIN} -e path_to_opentlc_key=${OPENTLC_KEY_PATH} -e param_repo_base=${JQ_REPO_BASE} -e opentlc_password=${OPENTLC_PASSWORD} -e REGION_NAME=${REGION} -e EMAIL=${RH_MAIL_ID} -e github_repo=${GITHUB_REPO}
+
+# EXECUTE SITE-OSP-INSTANCES PLAYBOOK
+#ansible-playbook site-osp-instances.yml -e tower_GUID=${TOWER_GUID} -e osp_GUID=${OSP_GUID} -e opentlc_login=${OPENTLC_LOGIN} -e path_to_opentlc_key=${OPENTLC_KEY_PATH} -e param_repo_base=${JQ_REPO_BASE} -e opentlc_password=${OPENTLC_PASSWORD} -e REGION_NAME=${REGION} -e EMAIL=${RH_MAIL_ID} -e github_repo=${GITHUB_REPO}
