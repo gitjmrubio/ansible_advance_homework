faf8a2be689ca6bb767678169cb11d68c085e942
JUAN MIGUEL RUBIO ROMAN <juan.miguel.rubio@gmail.com>
2020-02-28 21:35:23 +0000

    ADDED OTHER MODIFICATIONS ON ROLES

--- site-env-credentials.sh
+++ site-env-credentials.sh
@@ -20,7 +20,7 @@ RH_MAIL_ID="miguel.rubioroman"
 #ANSIBLE_ADVANCE_GUID=""
 
 # EXECUTE SITE-CONFIG-TOWER PLAYBOOK
-ansible-playbook site-config-tower.yml -e tower_GUID=${TOWER_GUID} -e osp_GUID=${OSP_GUID} -e opentlc_login=${OPENTLC_LOGIN} -e path_to_opentlc_key=${OPENTLC_KEY_PATH} -e param_repo_base=${JQ_REPO_BASE} -e opentlc_password=${OPENTLC_PASSWORD} -e REGION_NAME=${REGION} -e EMAIL=${RH_MAIL_ID} -e github_repo=${GITHUB_REPO}
+#ansible-playbook site-config-tower.yml -e tower_GUID=${TOWER_GUID} -e osp_GUID=${OSP_GUID} -e opentlc_login=${OPENTLC_LOGIN} -e path_to_opentlc_key=${OPENTLC_KEY_PATH} -e param_repo_base=${JQ_REPO_BASE} -e opentlc_password=${OPENTLC_PASSWORD} -e REGION_NAME=${REGION} -e EMAIL=${RH_MAIL_ID} -e github_repo=${GITHUB_REPO}
 
 # EXECUTE SITE-OSP-INSTANCES PLAYBOOK
-#ansible-playbook site-osp-instances.yml -e tower_GUID=${TOWER_GUID} -e osp_GUID=${OSP_GUID} -e opentlc_login=${OPENTLC_LOGIN} -e path_to_opentlc_key=${OPENTLC_KEY_PATH} -e param_repo_base=${JQ_REPO_BASE} -e opentlc_password=${OPENTLC_PASSWORD} -e REGION_NAME=${REGION} -e EMAIL=${RH_MAIL_ID} -e github_repo=${GITHUB_REPO}
+ansible-playbook site-osp-instances.yml -e tower_GUID=${TOWER_GUID} -e osp_GUID=${OSP_GUID} -e opentlc_login=${OPENTLC_LOGIN} -e path_to_opentlc_key=${OPENTLC_KEY_PATH} -e param_repo_base=${JQ_REPO_BASE} -e opentlc_password=${OPENTLC_PASSWORD} -e REGION_NAME=${REGION} -e EMAIL=${RH_MAIL_ID} -e github_repo=${GITHUB_REPO}
